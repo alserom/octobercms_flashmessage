3ed83ea249589764a26a9a8c3af98d324fd9563a
Alexander Romanov <romanov-acc@users.noreply.github.com>
2016-06-16 07:30:12 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,33 @@
 $(window).on('ajaxErrorMessage', function(event, message){
     event.preventDefault();
+    /*if(document.flashflag){
+        document.romanov_flashoptions.message = message;
+        document.romanov_flashsettings.type = 'danger';
+        $.notify(document.romanov_flashoptions, document.romanov_flashsettings);
+    }else{
+        $.notify({
+            message: message
+        },{
+            type: 'danger',
+            placement: {
+                from: "top",
+                align: "center"
+            },
+            delay: 10000
+        });
+    }*/
 });
 
 $(window).on('ajaxError', function(event, obj, context, status) {
     var message;
     var showfm = function(message){
         if(document.flashflag){
-            document.romanov_flashoptions.message = message[key][0];
+            document.romanov_flashoptions.message = message;
             document.romanov_flashsettings.type = 'danger';
             $.notify(document.romanov_flashoptions, document.romanov_flashsettings);
         }else{
             $.notify({
-                message: message[key][0]
+                message: message
             },{
                 type: 'danger',
                 placement: {
@@ -21,17 +37,24 @@ $(window).on('ajaxError', function(event, obj, context, status) {
                 delay: 10000
             });
         }
-    }
+    };
     if (typeof status.responseJSON === 'undefined') {
-        message = status.responseText.match(/^\"(.*)\"\s/)[1];
+        if (typeof status.responseText !== 'undefined') {
+            message = status.responseText.match(/^\"(.*)\"\s/);
+            if (message && typeof message[1] !== 'undefined') {
+                message = message[1];
+            } else {
+                message = status.responseText;
+            }
+        }
     } else {
         message = status.responseJSON.X_OCTOBER_ERROR_FIELDS;
     }
     if( typeof message === 'object' ) {
-        for (var key in message) {
-            showfm(message);
-        }
-    }else{
+        $.each(message, function(){
+            showfm(this[0]);
+        });
+    }else if(typeof message !== 'undefined'){
         showfm(message);
     }
 });
