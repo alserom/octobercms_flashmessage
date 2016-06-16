79dfd119b11450bb50f513352ca7b1cd618e56aa
Szabó Gergő <gergo85@gmail.com>
2016-06-16 12:10:41 +0200

    Minor bugfix and typo changes

--- Plugin.php
+++ Plugin.php
@@ -5,14 +5,14 @@ use Backend;
 
 class Plugin extends PluginBase
 {
-
     public function pluginDetails()
     {
         return [
             'name'        => 'romanov.flashmessage::lang.plugin.name',
             'description' => 'romanov.flashmessage::lang.plugin.description',
             'author'      => 'Alexander Romanov',
-            'icon'        => 'icon-bullhorn'
+            'icon'        => 'icon-bullhorn',
+            'homepage'    => 'https://github.com/romanov-acc/octobercms_flashmessage'
         ];
     }
 
@@ -23,12 +23,11 @@ class Plugin extends PluginBase
                 'label'       => 'romanov.flashmessage::lang.plugin.name',
                 'description' => 'romanov.flashmessage::lang.plugin.description',
                 'icon'        => 'icon-bullhorn',
-                'url'       => Backend::url('romanov/flashmessage/settings'),
+                'url'         => Backend::url('romanov/flashmessage/settings'),
                 'order'       => 501,
                 'category'    => 'system::lang.system.categories.cms',
-                'keywords'    => 'flash message',
-                'homepage'    => 'https://github.com/romanov-acc/octobercms_flashmessage'
-            ],
+                'keywords'    => 'flash message'
+            ]
         ];
     }
 
@@ -36,8 +35,7 @@ class Plugin extends PluginBase
     {
         return [
             'Romanov\Flashmessage\Components\FlashMessage' => 'flashmessage',
-            'Romanov\Flashmessage\Components\FormError' => 'formerrormessages',
+            'Romanov\Flashmessage\Components\FormError'    => 'formerrormessages'
         ];
     }
-
 }
